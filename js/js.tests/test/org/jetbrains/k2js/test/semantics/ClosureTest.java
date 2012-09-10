668946a7704495c294501e6d71ddf3ccddf19530
develar <develar@gmail.com>
2012-09-10 14:59:56 +0400

    fix object captured variables collections merge objectAsConstructorParameter and localParameterInLocalNamedFunction

--- js/js.tests/test/org/jetbrains/k2js/test/semantics/ClosureTest.java
+++ js/js.tests/test/org/jetbrains/k2js/test/semantics/ClosureTest.java
@@ -39,11 +39,8 @@ public final class ClosureTest extends SingleFileTranslationTest {
         fooBoxTest();
     }
 
+    // also test ObjectAsConstructorParameter
     public void testLocalParameterInLocalNamedFunction() throws Exception {
-        fooBoxTest();
-    }
-
-    public void testObjectAsConstructorParameter() throws Exception {
         fooBoxTest();
     }
 }
