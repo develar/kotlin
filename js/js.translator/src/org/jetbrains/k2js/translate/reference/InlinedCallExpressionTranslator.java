bf48038d232099b3a83679844bd63dfc2834e5f1
develar <develar@gmail.com>
2012-11-02 15:32:04 +0400

    use correct context while get name for fun param, make FunctionBodyTranslator static

--- js/js.translator/src/org/jetbrains/k2js/translate/reference/InlinedCallExpressionTranslator.java
+++ js/js.translator/src/org/jetbrains/k2js/translate/reference/InlinedCallExpressionTranslator.java
@@ -17,7 +17,10 @@
 package org.jetbrains.k2js.translate.reference;
 
 import com.google.common.collect.Maps;
-import com.google.dart.compiler.backend.js.ast.*;
+import com.google.dart.compiler.backend.js.ast.JsExpression;
+import com.google.dart.compiler.backend.js.ast.JsLiteral;
+import com.google.dart.compiler.backend.js.ast.JsNode;
+import com.google.dart.compiler.backend.js.ast.JsReturn;
 import com.intellij.util.SmartList;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -30,6 +33,7 @@ import org.jetbrains.jet.lang.psi.JetFunction;
 import org.jetbrains.jet.lang.resolve.calls.model.ResolvedValueArgument;
 import org.jetbrains.k2js.translate.context.TemporaryVariable;
 import org.jetbrains.k2js.translate.context.TranslationContext;
+import org.jetbrains.k2js.translate.expression.FunctionTranslator;
 import org.jetbrains.k2js.translate.utils.JsAstUtils;
 import org.jetbrains.k2js.translate.utils.mutator.LastExpressionMutator;
 import org.jetbrains.k2js.translate.utils.mutator.Mutator;
@@ -39,7 +43,6 @@ import java.util.Map;
 
 import static org.jetbrains.k2js.translate.reference.CallParametersResolver.resolveCallParameters;
 import static org.jetbrains.k2js.translate.utils.BindingUtils.getFunctionForDescriptor;
-import static org.jetbrains.k2js.translate.utils.FunctionBodyTranslator.translateFunctionBody;
 import static org.jetbrains.k2js.translate.utils.JsDescriptorUtils.getExpectedReceiverDescriptor;
 import static org.jetbrains.k2js.translate.utils.JsDescriptorUtils.getExpectedThisDescriptor;
 
@@ -75,7 +78,7 @@ public final class InlinedCallExpressionTranslator extends AbstractCallExpressio
     @NotNull
     private JsExpression translate() {
         TranslationContext contextWithAllParametersAliased = createContextForInlining();
-        JsNode translatedBody = translateFunctionBody(getFunctionDescriptor(), getFunctionBody(), contextWithAllParametersAliased);
+        JsNode translatedBody = FunctionTranslator.translateBody(getFunctionDescriptor(), getFunctionBody(), contextWithAllParametersAliased);
         //TODO: declare uninitialized temporary
         TemporaryVariable temporaryVariable = contextWithAllParametersAliased.declareTemporary(JsLiteral.NULL);
         JsNode mutatedBody = LastExpressionMutator.mutateLastExpression(translatedBody, new InlineFunctionMutator(temporaryVariable));
