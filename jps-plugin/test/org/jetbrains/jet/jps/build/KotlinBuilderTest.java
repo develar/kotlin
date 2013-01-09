35f9502df3eeaca2688102e794487a259f25242a
develar <develar@gmail.com>
2013-01-09 22:13:58 +0400

    KT-3071 tests

--- jps-plugin/test/org/jetbrains/jet/jps/build/KotlinBuilderTest.java
+++ jps-plugin/test/org/jetbrains/jet/jps/build/KotlinBuilderTest.java
@@ -16,28 +16,25 @@
 
 package org.jetbrains.jet.jps.build;
 
-import com.intellij.openapi.util.io.FileUtil;
 import org.jetbrains.jet.jps.model.JpsJsCompilerOutputPackagingElement;
 import org.jetbrains.jet.jps.model.JpsJsExtensionService;
 import org.jetbrains.jet.jps.model.JsExternalizationConstants;
 import org.jetbrains.jet.utils.PathUtil;
 import org.jetbrains.jps.incremental.artifacts.ArtifactBuilderTestCase;
+import org.jetbrains.jps.incremental.artifacts.ModuleBuilder;
 import org.jetbrains.jps.model.JpsModuleRootModificationUtil;
 import org.jetbrains.jps.model.artifact.JpsArtifact;
+import org.jetbrains.jps.model.artifact.elements.JpsPackagingElement;
 import org.jetbrains.jps.model.java.JpsJavaLibraryType;
 import org.jetbrains.jps.model.library.JpsLibrary;
 import org.jetbrains.jps.model.library.JpsOrderRootType;
 import org.jetbrains.jps.model.module.JpsModule;
 
-import java.io.File;
 import java.io.IOException;
 
 import static com.intellij.util.io.TestFileSystemBuilder.fs;
-import static org.jetbrains.jps.incremental.artifacts.LayoutElementTestUtil.root;
 
 public class KotlinBuilderTest extends ArtifactBuilderTestCase {
-    private static final String SHARED_TEST_SOURCE_FILES_PATH = AbstractKotlinJpsBuildTestCase.TEST_DATA_PATH + "shared";
-
     @Override
     public void setUp() throws Exception {
         super.setUp();
@@ -50,37 +47,43 @@ public class KotlinBuilderTest extends ArtifactBuilderTestCase {
         super.tearDown();
     }
 
-    private static void copyToSource(String filename, String sourceRoot) throws IOException {
-        FileUtil.copy(new File(SHARED_TEST_SOURCE_FILES_PATH + "/" + filename), new File(sourceRoot, filename));
-    }
-
     public void testSingleModule() throws IOException {
-        String sourceRoot = getAbsolutePath("src");
-        copyToSource("a.kt", sourceRoot);
-        String moduleName = "m";
-        JpsArtifact artifact = createModuleAndArtifact(moduleName, sourceRoot);
+        ModuleBuilder main = createModuleBuilder();
+        JpsArtifact artifact = main.copy("a.kt").createArtifact();
         rebuildAll();
-        assertOutput(artifact, fs().file(moduleName + ".js"));
+        assertOutput(artifact, fs().file(main.getName() + ".js"));
     }
 
-    private JpsArtifact createModuleAndArtifact(String moduleName, String srcRoot) {
-        return addArtifact(moduleName, root().element(gwtOutput(createModule(moduleName, srcRoot))));
+    private ModuleBuilder createModuleBuilder() {
+        return new MyModuleBuilder(this);
     }
 
-    private static JpsJsCompilerOutputPackagingElement gwtOutput(JpsModule m) {
-        return new JpsJsCompilerOutputPackagingElement(m.createReference());
+    private static class MyModuleBuilder extends ModuleBuilder {
+        public MyModuleBuilder(KotlinBuilderTest testCase) {
+            super(JsBuilder.NAME, testCase, AbstractKotlinJpsBuildTestCase.TEST_DATA_PATH + "shared");
+        }
+
+        @Override
+        protected void moduleCreated(JpsModule module) {
+            JpsLibrary library = module.addModuleLibrary(JsExternalizationConstants.JS_LIBRARY_NAME, JpsJavaLibraryType.INSTANCE);
+            library.addRoot(PathUtil.getKotlinPathsForDistDirectory().getRuntimePath(false), JpsOrderRootType.SOURCES);
+            JpsModuleRootModificationUtil.addDependency(module, library);
+
+            JpsJsExtensionService.getInstance().setExtension(module);
+        }
+
+        @Override
+        protected JpsPackagingElement createPackagingElement(JpsModule module) {
+            return new JpsJsCompilerOutputPackagingElement(module.createReference());
+        }
     }
 
-    private JpsModule createModule(String moduleName, String srcRoot) {
-        JpsModule module = addModule(moduleName, srcRoot);
-        addCompilerLibrary(module);
-        JpsJsExtensionService.getInstance().setExtension(module);
-        return module;
-    }
-
-    private static void addCompilerLibrary(JpsModule module) {
-        JpsLibrary library = module.addModuleLibrary(JsExternalizationConstants.JS_LIBRARY_NAME, JpsJavaLibraryType.INSTANCE);
-        library.addRoot(PathUtil.getKotlinPathsForDistDirectory().getRuntimePath(false), JpsOrderRootType.SOURCES);
-        JpsModuleRootModificationUtil.addDependency(module, library);
-    }
+    //public void testMultiModule() throws IOException {
+    //    String sourceRoot = getAbsolutePath("src");
+    //    copyToSource("a.kt", sourceRoot);
+    //    String moduleName = "m";
+    //    JpsArtifact artifact = createModuleAndArtifact(moduleName, sourceRoot);
+    //    rebuildAll();
+    //    assertOutput(artifact, fs().file(moduleName + ".js"));
+    //}
 }
